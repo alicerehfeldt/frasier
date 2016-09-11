bbceeb32876ee2b7177fca979753b053c66cb35e
Alice Rehfeldt <ojamarushealice@yahoo.com>
2016-09-10 19:46:14 -0700

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: '',
+  model_path: __dirname + '/../model/frasier.json',
+  from_user: '',
+  filter: /^FRASIER FACTS:/
+};
